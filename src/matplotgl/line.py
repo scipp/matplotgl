2c94a19845dd04fdd5e771fcdf3c1ad3e24f7621
Neil Vaytet <neil.vaytet@ess.eu>
2025-11-09 22:07:32 +0100

    add semilogx, semilog, and loglog, and fix some log scale issues

--- src/matplotgl/line.py
+++ src/matplotgl/line.py
@@ -10,19 +10,27 @@ from .utils import find_limits, fix_empty_range
 
 
 class Line:
-    def __init__(self, x, y, fmt="-", color="C0", ls="solid", lw=1, ms=5, zorder=0):
+    def __init__(
+        self,
+        x,
+        y,
+        fmt="-",
+        color="C0",
+        ls="solid",
+        lw=1,
+        ms=5,
+        zorder=0,
+        xscale="linear",
+        yscale="linear",
+    ):
         self.axes = None
-        self._xscale = "linear"
-        self._yscale = "linear"
+        self._xscale = xscale
+        self._yscale = yscale
         self._x = np.asarray(x)
         self._y = np.asarray(y)
         self._zorder = zorder
-        self._line_geometry = p3.LineGeometry(
-            positions=np.array(
-                [self._x, self._y, np.full_like(self._x, self._zorder - 50)],
-                dtype="float32",
-            ).T
-        )
+        pos = self._make_positions()
+        self._line_geometry = p3.LineGeometry(positions=pos)
 
         self._color = mplc.to_hex(color)
         self._line = None
@@ -39,16 +47,7 @@ class Line:
         if "o" in fmt:
             self._vertices_geometry = p3.BufferGeometry(
                 attributes={
-                    "position": p3.BufferAttribute(
-                        array=np.array(
-                            [
-                                self._x,
-                                self._y,
-                                np.full_like(self._x, self._zorder - 50),
-                            ],
-                            dtype="float32",
-                        ).T
-                    ),
+                    "position": p3.BufferAttribute(array=pos),
                 }
             )
             self._vertices_material = p3.PointsMaterial(color=self._color, size=ms)
@@ -70,14 +69,18 @@ class Line:
             out.append(self._vertices)
         return p3.Group(children=out) if len(out) > 1 else out[0]
 
-    def _update(self):
+    def _make_positions(self):
         with warnings.catch_warnings(category=RuntimeWarning, action="ignore"):
             xx = self._x if self._xscale == "linear" else np.log10(self._x)
             yy = self._y if self._yscale == "linear" else np.log10(self._y)
         pos = np.array(
-            [xx, yy, np.full_like(xx, self._zorder - 50)],
+            [xx, yy, np.full_like(xx, self._zorder)],
             dtype="float32",
         ).T
+        return pos
+
+    def _update(self):
+        pos = self._make_positions()
         if self._line is not None:
             self._line_geometry.positions = pos
         if self._vertices is not None:
